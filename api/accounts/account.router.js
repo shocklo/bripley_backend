02113627f65a0553ebe8ca1e786db1b59e8b815a
Cristobal <shocklo@hotmail.com>
2021-01-11 22:01:38 -0300

    new ws to router

--- api/accounts/account.router.js
+++ api/accounts/account.router.js
@@ -1,4 +1,4 @@
-const { getTransfers, doTransfer, doDeposit, doWithdraw } = require("./account.controller");
+const { getTransfers, doTransfer, doDeposit, doWithdraw, myAccount } = require("./account.controller");
 const router = require("express").Router();
 const { checkToken } = require("../../auth/jwt_validator");
 
@@ -6,6 +6,8 @@ router.get("/", checkToken, getTransfers);
 router.post("/transfer", checkToken, doTransfer);
 router.post("/doDeposit", checkToken, doDeposit);
 router.post("/doWithdraw", checkToken, doWithdraw);
+router.post("/myAccount", checkToken, myAccount);
+
 
 module.exports = router;
 
